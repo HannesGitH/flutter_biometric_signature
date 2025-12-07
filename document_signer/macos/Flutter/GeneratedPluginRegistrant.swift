f9c98fb4719af48e476210c882ed4c4d3e427c43
chamodanethra <chamodananayakkara@gmail.com>
2025-12-07 19:38:57 +0530

    feat(example): Add macOS support to document_signer app
    This commit adds macOS support to the `document_signer` example application. A complete macOS project structure, including Xcode configurations, entitlements, and source files, has been created.
    
    The main `example` app's macOS project was also updated to remove a hardcoded development team.

---
+++ document_signer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import biometric_signature
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  BiometricSignaturePlugin.register(with: registry.registrar(forPlugin: "BiometricSignaturePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
