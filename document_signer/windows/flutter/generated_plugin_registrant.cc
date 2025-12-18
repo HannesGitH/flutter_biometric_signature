57b316efcd2e40061cb54f37a006b512de1fc7a8
chamodanethra <chamodananayakkara@gmail.com>
2025-12-18 19:44:02 +0530

    Add Windows platform support for other example apps.

---
+++ document_signer/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <biometric_signature/biometric_signature_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  BiometricSignaturePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("BiometricSignaturePlugin"));
+}
